2e7361aab0f4458e279fcecd417ff93ff1a7c98f
xylae <itsxylae@gmail.com>
2025-04-28 23:22:50 +0800

    Add files via upload

---
+++ color.lua
@@ -0,0 +1,22 @@
+---@class LacklusterColor
+return {
+    lack = "#708090",
+    luster = "#DEEEED",
+    orange = "#6F5050",
+    yellow = "#A49D66",
+    green = "#708070",
+    blue = "#7788AA",
+    red = "#B68464",
+    none = "none",
+
+    black = "#000000",
+    gray1 = "#080808",
+    gray2 = "#191919",
+    gray3 = "#2a2a2a",
+    gray4 = "#444444",
+    gray5 = "#555555",
+    gray6 = "#7a7a7a",
+    gray7 = "#aaaaaa",
+    gray8 = "#cccccc",
+    gray9 = "#DDDDDD",
+}
