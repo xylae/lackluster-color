320200dedf272f459b0a963a407e303ebeecccb5
xylae <itsxylae@gmail.com>
2025-05-03 17:13:14 +0000

    Update create-theme.lua

--- create-theme.lua
+++ create-theme.lua
@@ -11,7 +11,7 @@ return function(color, color_special)
         use_undercurl = true,
         fg_end_of_buffer = color_special.main_background,
         fg_normal = color.gray8,
-        bg_normal = color_special.main_background,
+        bg_normal = color.black,
         fg_whitespace = color_special.whitespace,
 
         fg_icon = color.gray4,
